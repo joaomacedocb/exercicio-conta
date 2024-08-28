2118f0bf2e79d55b01bee14e4deaed8f7648c144
joaomacedocb <joaomacedocb@gmail.com>
2024-08-28 17:16:20 -0300

    Adicionado classe conta bancaria ao exercicio.

--- src/main/java/org/example/ContaBancaria.java
+++ src/main/java/org/example/ContaBancaria.java
@@ -1,4 +1,57 @@
 package org.example;
 
 public class ContaBancaria {
+    private int conta;
+    private String agencia;
+    private String nomeCliente;
+    private boolean saldoConta;
+
+    public ContaBancaria(int conta, String agencia, String nomeCliente, boolean saldoConta) {
+        this.conta = conta;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldoConta = saldoConta;
+    }
+
+    public int getConta() {
+        return conta;
+    }
+
+    public void setConta(int conta) {
+        this.conta = conta;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public boolean isSaldoConta() {
+        return saldoConta;
+    }
+
+    public void setSaldoConta(boolean saldoConta) {
+        this.saldoConta = saldoConta;
+    }
+
+    @Override
+    public String toString() {
+        return "ContaBancaria{" +
+                "conta=" + conta +
+                ", agencia='" + agencia + '\'' +
+                ", nomeCliente='" + nomeCliente + '\'' +
+                ", saldoConta=" + saldoConta +
+                '}';
+    }
 }
